c35e10e176619d69289f0659f391aa32c52ce27d
carlos rocha <carlosandrade399@gmail.com>
2023-04-23 21:34:30 -0300

    feat: exibindo resultado

--- jogo.py
+++ jogo.py
@@ -1,9 +1,12 @@
 import cv2
 import mediapipe as mp
 import constants
+import math
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+font_scale = 1
 
 def setMove(hand):
-
     if hand == constants.LEFT:
         if (landmark[6].x < landmark[8].x) and (landmark[10].x < landmark[12].x) and (landmark[14].x < landmark[16].x):
             return constants.PAPEL
@@ -13,13 +16,24 @@ def setMove(hand):
             return constants.PEDRA
     elif hand == constants.RIGHT:
         if (landmark[6].x > landmark[8].x) and (landmark[10].x > landmark[12].x) and (landmark[14].x > landmark[16].x):
-            cv2.putText(video, constants.PAPEL, (w-200,100), cv2.FONT_HERSHEY_SIMPLEX, 1 , (0,0,0), 2, cv2.LINE_AA )
+            return constants.PAPEL
         elif (landmark[6].x > landmark[8].x) and (landmark[10].x > landmark[12].x):
-            cv2.putText(video, constants.TESOURA, (w-200,100), cv2.FONT_HERSHEY_SIMPLEX, 1 , (0,0,0), 2, cv2.LINE_AA )
+            return constants.TESOURA
         elif (landmark[6].x < landmark[8].x):
-            cv2.putText(video, constants.PEDRA, (w-200,100), cv2.FONT_HERSHEY_SIMPLEX, 1 , (0,0,0), 2, cv2.LINE_AA )
-    
+            return constants.PEDRA
 
+def setMatchWinner(move_player_1, move_player_2):
+    if move_player_1 == move_player_2:
+        return "Empate!"
+    elif move_player_1 == "pedra" and move_player_2 == "tesoura":
+        return "Jogador 1"
+    elif move_player_1 == "papel" and move_player_2 == "pedra":
+        return "Jogador 1"
+    elif move_player_1 == "tesoura" and move_player_2 == "papel":
+        return "Jogador 1"
+    else:
+        return "Jogador 2"
+  
 # instanciando variaveis
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
@@ -30,6 +44,9 @@ hands = mp_hands.Hands(
     min_tracking_confidence=0.3,
     max_num_hands=2
 )
+
+move_player_1 = ""
+move_player_2 = ""
 
 # capturando video do desafio
 video_capture = cv2.VideoCapture("pedra-papel-tesoura.mp4")
@@ -48,12 +65,6 @@ while video_capture.isOpened():
     video.flags.writeable = True
     video = cv2.cvtColor(video, cv2.COLOR_RGB2BGR)
 
-    score_player_1 = 0
-    score_player_2 = 0
-    move_player_1 = ""
-    move_player_2 = ""
-
-
     hand_landmarks = results.multi_hand_landmarks
     if hand_landmarks and len(hand_landmarks) == 2:
 
@@ -65,35 +76,21 @@ while video_capture.isOpened():
             if (landmark[0].x < landmark[12].x):
                 # Mão esquerda
                 move_player_1 = setMove(constants.LEFT)
-                cv2.putText(video, "Jogador 1", (50,70), cv2.FONT_HERSHEY_SIMPLEX, 1 , (0,0,0), 2, cv2.LINE_AA)
-                cv2.putText(video, move_player_1, (50,100), cv2.FONT_HERSHEY_SIMPLEX, 1 , (0,0,0), 2, cv2.LINE_AA)
+                cv2.putText(video, "Jogador 1", (50,70), font, font_scale, (0,0,0), 2, cv2.LINE_AA)
+                cv2.putText(video, move_player_1, (50,100), font, font_scale, (0,0,0), 2, cv2.LINE_AA)
                 
             else:
                 # Mão direita
                 move_player_2 = setMove(constants.RIGHT)                
-                cv2.putText(video, move_player_2, (w-200,100), cv2.FONT_HERSHEY_SIMPLEX, 1 , (0,0,0), 2, cv2.LINE_AA )
-                cv2.putText(video, "Jogador 2", (w-200,70), cv2.FONT_HERSHEY_SIMPLEX, 1 , (0,0,0), 2, cv2.LINE_AA)
-     
-            if move_player_1 == move_player_2:
-                print("Empate!")
-            # elif move_player_1 == "pedra":
-            #     if move_player_2 == "tesoura":
-            #         print("Jogador 1 ganha!")
-            #     else:
-            #         print("Jogador 2 ganha!")
-            # elif move_player_1 == "papel":
-            #     if move_player_2 == "pedra":
-            #         print("Jogador 1 ganha!")
-            #     else:
-            #         print("Jogador 2 ganha!")
-            # elif move_player_1 == "tesoura":
-            #     if move_player_2 == "papel":
-            #         print("Jogador 1 ganha!")
-            #     else:
-            #         print("Jogador 2 ganha!")           
+                cv2.putText(video, "Jogador 2", (w-200,70), font, font_scale, (0,0,0), 2, cv2.LINE_AA)
+                cv2.putText(video, move_player_2, (w-200,100), font, font_scale, (0,0,0), 2, cv2.LINE_AA)    
 
+            winner = setMatchWinner(move_player_1, move_player_2)
+            cv2.getTextSize(winner, font, fontScale=font_scale, thickness=1)[0]
+            cv2.putText(video, f'Resultado: {winner}', (320,(h-100)), font, font_scale, (0,0,0), 2, cv2.LINE_AA)
+            
+            
     cv2.imshow("Game - Rock, Paper, Scissors", video)
-
 
    # Esperar pela tecla 'q' para sair
     if cv2.waitKey(1) & 0xFF == ord('q'):
